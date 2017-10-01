e53bc9ac45fa07ba9a828b6c7657c5b920248f27
omegadeep10 <omegadeep10@outlook.com>
2017-10-01 19:26:01 -0400

    Add basic home page layout and styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="hello">
-    <h1>Logged In: {{ isLoggedIn() }}</h1>
-    <p>Home Page </p>
-    <pre>{{ courses }}</pre>
+  <div class="home-container">
+    <h1>Welcome Scott Spangler. Let's Assess Your Students.</h1>
+    <p class="subtext">Please select one of your incomplete classes or add a new one.</p>
+    <div class="courses">
+        <h3>Your Classes</h3>
+        <div class="course" v-for="course in courses" :key="course.crn">
+            <div class="course-info">
+                <h4>{{ course.course_name }}</h4>
+                <p><span>{{ course.crn }} - </span>{{ course.semester }} {{ course.course_year }}</p>
+            </div>
+            <div class="assessment-info">
+                <p class="completed">12/46</p>
+                <span>Assessments Completed</span>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -36,4 +48,95 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="sass">
+@import '../styles/variables'
+
+.home-container
+    text-align: center
+    padding-top: 100px
+    height: 100vh
+    background-color: #f1f1f1
+
+h1
+    text-align: center
+    text-transform: uppercase
+    font-family: "Roboto"
+    font-weight: bold
+    color: #333
+    font-size: 26px
+
+.subtext
+    text-align: center
+    font-family: "Roboto"
+    font-weight: 400
+    color: #888
+    font-size: 17px
+    margin-top: 10px
+
+.courses
+    padding: 20px 30px
+    max-width: 900px
+    margin: 100px auto 0 auto
+
+    h3
+        font-family: "Roboto"
+        font-size: 20px
+        text-align: left
+        color: #444
+        font-weight: 500
+        margin-bottom: 30px
+
+.course
+    background-color: white
+    text-align: left
+    margin-bottom: 15px
+    padding: 25px 30px
+    border: 1px solid #cfcfcf
+    transition: all .2s ease
+
+    &:hover
+        cursor: pointer
+        border: 1px solid $mga-primary
+
+    .course-info
+        display: inline-block
+        width: 50%
+
+        h4
+            font-family: "Roboto"
+            font-weight: 500
+            font-size: 18px
+            color: #434343
+        
+        p
+            font-family: "Roboto"
+            font-weight: 500
+            font-size: 14px
+            margin-top: 10px
+            color: #434343
+
+            span
+                color: #919191
+
+    .assessment-info
+        display: inline-block
+        text-align: right
+        width: 49%
+        vertical-align: top
+
+        .completed
+            font-family: "Roboto"
+            font-weight: 700
+            font-size: 24px
+            color: #434343
+        
+        span
+            font-family: "Roboto"
+            font-weight: 400
+            font-size: 12px
+            font-style: italic
+            color: #ababab
+            display: inline-block
+            margin-top: 5px
+
+
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,13 +8,12 @@
       </div>
 
       <form>
+        <p class="error">{{ formError }}</p>
         <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" v-model="email">
+          <input type="email" v-model="email" placeholder="Email">
         </div>
         <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" v-model="password">
+          <input type="password" v-model="password" placeholder="Password">
         </div>
         <button type="submit" v-on:click="handleLogin()">Submit</button>
       </form>
@@ -36,6 +35,7 @@ export default {
   },
   methods: {
       handleLogin() {
+          this.formError = "";
           login(this.email, this.password)
             .then(x => this.$router.push('/'))
             .catch(err => this.formError = err);
@@ -61,13 +61,15 @@ export default {
 .header 
   .logo
     max-width: 200px
+    display: block
+    margin: 0 auto
   
   h1
     font-family: "Roboto"
     font-weight: 700
     text-align: center
     font-size: 18px
-    color: $mga-dark
+    color: $mga-primary
     margin-top: 30px
   
   p
@@ -75,10 +77,49 @@ export default {
     font-weight: 400
     font-size: 14px
     color: #777
-    margin-top: -5px
+    margin-top: 5px
     text-align: center
 
 form
-  background-color: $mga-grey
+  margin-top: 20px
+  text-align: center
+  padding: 20px
+
+  input
+    width: 250px
+    font-family: "Roboto"
+    font-size: 14px
+    font-weight: 500
+    padding: 10px 15px
+    border: 0
+    background-color: #F5F5F5
+    border: 1px solid #DCDCDC
+    color: #404040
+  
+  input[type=email]
+    margin-bottom: 5px
+  
+  button
+    width: 250px
+    border: 1px solid $mga-dark
+    background-color: $mga-primary
+    color: white
+    font-family: "Roboto"
+    font-size: 14px
+    font-weight: 500
+    text-align: center
+    padding: 10px 15px
+    margin-top: 15px
+
+    &:hover
+      background-color: $mga-dark
+      cursor: pointer
+    
+  .error
+    font-family: "Roboto"
+    font-size: 14px
+    font-weight: 500
+    color: #cc0000
+    margin-bottom: 15px
 
 </style>
